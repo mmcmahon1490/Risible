2604a27d16302467929ecce26db8b072e4029dc6
Michael McMahon <michaelmcmahon@Mac-mini.fios-router.home>
2020-09-10 01:08:21 -0400

    Working Through Create

--- risible/src/Components/CreatePostForm.jsx
+++ risible/src/Components/CreatePostForm.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from './Footer';
+import axios from 'axios';
 
 const CreatePostForm = () => {
+  const [link, setLink] = useState('');
+  const [description, setDescription] = useState('');
+  const [user, setUser] = useState('');
+  const [date, setDate] = useState('');
+  const baseURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/risible`;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const fields = {
+      link,
+      description,
+      user,
+      date,
+    };
+
+    await axios.post(
+      baseURL,
+      { fields },
+      {
+        headers: {
+          Authorization: `Bearer ${process.envREACT_APP_AIRTABLE_KEY}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  }
   
   return (
     <div>
@@ -11,6 +38,11 @@ const CreatePostForm = () => {
         <input type='lnk' placeholder='enter link' name='lnk' id='lnk' required></input>
         <label for='description'><b>description</b></label>
         <input type='dsc' placeholder='enter description' name='dsc' id='dsc' required></input>
+
+        <label for='user'><b>user</b></label>
+        <input type='usr' placeholder='enter username' name='usr' id='usr' required></input>
+        <label for='date'><b>enter date</b></label>
+        <input type='dte' placeholder='enter date' id='dte' required></input>
       </div>
       <button type='submit' class='submit-post'>submit</button>
       <Footer />
